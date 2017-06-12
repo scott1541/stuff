1213a7241ff1f6d8d42ec81c50d4510fffe167fb
Scott <scott3933@gmail.com>
2017-06-12 17:26:41 +0000

    Monday finish

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -3,7 +3,7 @@ package recfun
 object Main {
   def main(args: Array[String]) {
     println("Pascal's Triangle")
-    for (row <- 0 to 10) {
+    for (row <- 0 to 15) {
       for (col <- 0 to row)
         print(pascal(col, row) + " ")
       println()
